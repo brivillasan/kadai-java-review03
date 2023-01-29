99b97f9d7f0c63e7de0fc13061fc163fce083ce3
brivillasan <brivillayh@gmail.com>
2023-01-29 23:02:51 +0900

    クラス名を修正いたしました。ご確認のほど、よろしくお願いいたします。

--- src/baseball/Reviw03.java
+++ src/baseball/Reviw03.java
@@ -1,6 +1,6 @@
 package baseball;
 
-public class BaseBallTeamR {
+public class Reviw03 {
 
     public static void main(String[] args) {
         BaseBallTeam team1 = new BaseBallTeam("東京ヤクルトスワローズ",80,59,4);
@@ -9,7 +9,7 @@ public class BaseBallTeamR {
         BaseBallTeam team4 = new BaseBallTeam("読売ジャイアンツ",68,72,3);
         BaseBallTeam team5 = new BaseBallTeam("広島東洋カープ",66,74,3);
         BaseBallTeam team6 = new BaseBallTeam("中日ドラゴンズ",66,75,2);
-        
+
         team1.report();
         team2.report();
         team3.report();
